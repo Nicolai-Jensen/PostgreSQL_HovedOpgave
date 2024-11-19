b1037c8c6430dd024f7bd228b8baa90aad27d414
Nicolai-Jensen <74540161+Nicolai-Jensen@users.noreply.github.com>
2024-11-19 12:03:48 +0100

    Updated .sql with String commands

--- SQL_Scripts.sql
+++ SQL_Scripts.sql
@@ -2,3 +2,12 @@ SELECT * FROM "GameData";
 
 ALTER TABLE "GameData" 
     ALTER COLUMN "UserId" SET DEFAULT nextval('GameData_UserId_seq');
+
+
+
+
+
+pg_restore -U postgres -h localhost -p 5432 -d hovedOpgaveDatabase "path_to_your_dump_file.dump"
+
+
+pg_dump -U postgres -h localhost -p 5432 -d hovedOpgaveDatabase -F c -f "path_to_place\database_backup.dump"
