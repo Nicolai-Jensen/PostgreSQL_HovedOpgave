e023b2e1ac9872847276ca419a799b3c5da8c4ff
Nicolai-Jensen <74540161+Nicolai-Jensen@users.noreply.github.com>
2024-11-19 11:54:18 +0100

    Sql update

--- SQL_Scripts.sql
+++ SQL_Scripts.sql
@@ -1,8 +1,4 @@
-UPDATE game_data
-SET body = '{"gender": "female", "money": 150, "chips": 50, "volume": 75, "rewards": "none"}'
-WHERE game_id = 1;
+SELECT * FROM "GameData";
 
-INSERT INTO game_data (user_id, game_id, body)
-VALUES (2, 1, '{"gender": "male", "money": 10090, "chips": 50, "volume": 75, "rewards": "none"}');
-
-SELECT * FROM GameData;
+ALTER TABLE "GameData" 
+    ALTER COLUMN "UserId" SET DEFAULT nextval('GameData_UserId_seq');
